17552db0ea7d5145650029811fc290c3b5100b18
ali <alli.gorji@gmail.com>
2017-08-03 20:02:17 +0430

    Add mongodb support to both links and app crawling parts

--- itunes_app_store_scraper_multithread.py
+++ itunes_app_store_scraper_multithread.py
@@ -1,18 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        itunes_app_store_scraper_multithread.py
-# Purpose:     To crawl the itunes app store to put together a data set for
-#              individual apps
-#
-# Author:      Mike Laderman
-#
-# Created:     July 2012
-# Copyright:   (c) Mike Laderman 2012
-# 
-#-------------------------------------------------------------------------------
-
-
-#!/usr/local/bin/python3.2
-
 import urllib.request
 from urllib.parse import urlparse, parse_qs
 from bs4 import BeautifulSoup
@@ -22,6 +7,7 @@ import csv
 from random import shuffle
 import threading
 
+from pymongo import MongoClient
 #--------User Set These Global Variables----------#
 
 #Set operation to "store" in order to get the initial list of links to scrape
@@ -38,13 +24,7 @@ sleep = 0
 sample = ''
 
 #Number of threads spawned to call the itunes store at one time. Must be an integer.
-threads = 3
-
-#Set links_file to the name of the file with the initial list of links to scrape
-# Set app_info_file to the name of the file with the information for those links
-links_file = 'App_Store_Links.csv'
-app_info_file = 'App_Store_Info1.csv'
-
+threads = 2
 
 #--------DO NOT TOUCH!----------#
 nav_site = "http://itunes.apple.com/us/genre/ios-books/id6018?mt=8"
@@ -70,7 +50,7 @@ def site_open(site):
            pass
 
 
-def dict_get(soup):
+def dict_get(soup): #TODO make full dictionary of whole data including version and link(?)
     dic = {}
     try:
         dic['description'] = description_get(soup)
@@ -86,7 +66,7 @@ def dict_get(soup):
         dic['rating'] = rating_get(soup)
 
     except:
-        print("Error in dict generating")
+        print("Something is missing in the page.")
         pass
 
     return dic
@@ -153,26 +133,6 @@ def rating_get(soup):
     #return a tuple of stars and rating without whitespace
     return (stars.strip(),rating.strip())
 
-def app_info(soup,site):
-    '''Aggregates info from single app and prints it into a tuple.\n
-    Returns (title, dev, price, category, size, seller, stars, rating, url)'''
-    try:
-
-        #Makes a call to each subfunction and assigns value to local var
-        title = title_get(soup)
-        developer = dev_get(soup)
-        price = price_get(soup)
-        category = category_get(soup)
-        size = size_get(soup)
-        seller = seller_get(soup)
-        stars,rating = rating_get(soup)
-
-        #returns tuple of the app information
-        return (title,developer,price,category,size,seller,stars,rating,site)
-    except:
-        print("I Dunno, There Was Some Error!")
-        pass
-    
 
 def genre_link_list(site):
     '''Generator function that 
@@ -206,7 +166,12 @@ def app_link_list(site):
         #outputs tuple with the link that will be written to a csv file
         yield (link.get('href'),)
 
-def general_app_store_crawl(file_name,sleep_time=float):
+
+def insert_to_links_database(document, collection):
+    collection.update({"_id": document['_id']}, document,upsert=True)
+
+
+def general_app_store_crawl(collection,sleep_time=float):
     '''Creates list of iTunes app store links.
     Grabs the first page of links for the first letter of the alphabet
     for every genre and writes it to a csv file.
@@ -215,20 +180,14 @@ def general_app_store_crawl(file_name,sleep_time=float):
     1) file_name is the name of the output file_name
     2) sleep_time is a float value that indicates pause between site requests.
 
-    Output: A csv file that lists most US iTunes store apps'''
-
-    
-    #creates and/or appends csv file
-    f = open(file_name,'a',newline = '')
-    writer = csv.writer(f,dialect = 'excel',quoting=csv.QUOTE_NONNUMERIC)
+    Output: Mongodb :lists most US iTunes store apps'''
 
     #grabs everything from the database
     for i,link in enumerate(genre_link_list(nav_site)):
 
         print('Scraping genre '+ str(i) +'.')
-
         #loops through the alphabet to generate relevant sites
-        #and write to csv
+        #and insert to mongodb
         for letter in alphabet:
 
             #reconstructs URL
@@ -239,24 +198,24 @@ def general_app_store_crawl(file_name,sleep_time=float):
             time.sleep(sleep_time)
 
             #iteratively calls the app_link_list() function
-            #to write individual app links into the csv file
             for app in app_link_list(new_site):
-
-                #writes single link into a csv file line
-                writer.writerow(app)
-        
-    #closes file (duh)
-    f.close()
+                #make document for link database
+                link_document = {
+                    "_id": app[0].split('/')[-1][:-5],
+                    "address": app[0],
+                    "genre": str(link).split('/')[-2]
+                }
+                #add document to database
+                insert_to_links_database(link_document, collection)
     print('Completed Scrapping')
     return
 
-def read_in(file):
+def read_in_from_links_database(collection):
     '''reads in app store links from the app links file.
     Returns array.'''
-    f = open(file,'r')
-    data = f.readlines()
-
-    #return array of links
+    data=[]
+    for document in collection.find():
+        data.append(document)
     return data
 
 
@@ -272,66 +231,52 @@ def split_data(data,splits):
         new_data.append(j)
     return new_data
 
-def app_info_crawl(source,output,sleep_time=float,sample_size=None,num_threads=1):
+def app_info_crawl(source, output, sleep_time=float, sample_size=None, num_threads=1):
     '''Takes in Source URLs and outputs App Info
-
     Inputs:
     1) source is the name of the input file with the list of urls to crawl
     2) output is the name of the output file_name with full app information
     3) sleep_time is a float value that indicates pause between site requests.
     4) num_threads is the number of threads that the script spawns to speed up process.
 
-        
     Returns X number of csv files with app info, where X is equal to the number
     of threads used to grab app info. You can merge them all together in the command line
-    using the command "cat *.csv > output_file"'''
+    using the command "cat *.debug
+    csv > output_file"'''
 
-    #reads in list of apps
-    data = read_in(source)
+    # reads in list of apps from database
+    data = read_in_from_links_database(source)
 
-    #randomizes data order to get a random sample
-    shuffle(data)
+    # breaks up data into an array of smaller data arrays
+    # for multithreading purposes
+    data = split_data(data, num_threads)
 
-    #breaks up data into an array of smaller data arrays
-    #for multithreading purposes
-    data = split_data(data,num_threads)
-
-    #checks if sample_size is designated
-    if sample_size != '':
-
-        #checks if sample size is an integer and smaller than the size of the dataset
-        if isinstance(sample_size,int) and sample_size < len(data):
-
-            #makes data the size of the sample size
-            data = data[0:sample_size]
-        else:
-            raise ValueError('Your sample_size needs to be an integer and smaller than the size of the data set.')
-
-    #loops through data to create a new thread that will loop through the store and find 
-    #a link, crawl the site, and enter the data
-    #into the output csv
-    for i,link_list in enumerate(data):
-
-        #opens a csv file formatted as Links(i).csv according to the number of threads
-        f = open('Links'+str(i)+'.csv','a',newline = '')
-        writer = csv.writer(f,dialect = 'excel',quoting=csv.QUOTE_NONNUMERIC)
-
+    # into the output csv
+    for link_list in data:
         #spawns thread and starts scrapping
-        t = threading.Thread(target = app_crawl_main_loop,args = (link_list,writer) )
+        t = threading.Thread(target = app_crawl_main_loop,args = (output, link_list))
         t.start()
     print('Completed Spawning Threads')
     return
 
-def app_crawl_main_loop(data,writer):
+
+def insert_to_apps_database(collection, document): #TODO add update part to support adding new versions
+    collection.update({"_id": document['_id']}, document, upsert=True)
+
+
+def app_crawl_main_loop(collection, data):
     '''Called by a thread in app_info_crawl(). Loops through
     a sub-data array and writes output to a sub-csv file.'''
-    for i,link in enumerate(data):
-        
-        print("Scrapping #"+str(i)+".")
+    for link_document in data:
+        link = link_document.get("address")
+        print("Scrapping #"+str(link)+".")
         try:
-            info = app_info(soup_site(link.strip('"')),link.strip('"'))
+            # get a dictionary from app page to save in database
+            info_document = dict_get(soup_site(link))
+            info_document['_id'] = link.split('/')[-1][:-5]
+            info_document['link'] = link
             #opens the site, parses out the data, and writes it into the csv
-            writer.writerow(info)
+            insert_to_apps_database(collection, info_document)
         except:
             #continues loop of error is found and skips entry
             print("Could not write to csv file for some reason =0")
@@ -341,22 +286,28 @@ def app_crawl_main_loop(data,writer):
 
 def main():
     print(dict_get(soup_site("https://itunes.apple.com/us/app/trvl/id391961927?mt=8%22")))
-    exit()
 
     '''Main function that runs either the general app store crawler
     or the individual app crawler, depending on what "operation" is set to
     at the head of the script.'''
+    print("Waiting for mongodb connection")
+    client = MongoClient()
+    db = client.appstore
+    print("MongoClient connected successfully")
     
     #runs the main store crawl and prints out total time spent
     if operation == 'store':
+        links = db.links
         start_time = time.time()
-        general_app_store_crawl(links_file,sleep)
+        general_app_store_crawl(links,sleep)
         print(time.time()-start_time)
 
     #runs the app info crawl and prints out total time spent
     elif operation == 'apps':
+        apps = db.apps
+        links = db.links
         start_time = time.time()
-        app_info_crawl(links_file,app_info_file,sleep,sample,threads)
+        app_info_crawl(links, apps,sleep,sample,threads)
         print(time.time()-start_time)
     else:
         print('You need to set "operation" to "store" or "apps"!')       
